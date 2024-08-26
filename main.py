adb9b20bffb54e008970de2588510abbd010475d
lyj00912 <3122742760@qq.com>
2024-08-26 19:02:06 +0800

    created the first working version of markdown 2 html conversion.

---
+++ main.py
@@ -0,0 +1,20 @@
+import markdown
+
+# Markdown 文本
+markdown_text = """
+# 这是一个标题
+
+这是一些 *斜体* 和 **粗体** 的文本。
+
+- 列表项 1
+- 列表项 2
+"""
+
+# 转换为 HTML
+html = markdown.markdown(markdown_text)
+
+# 输出 HTML
+print(html)
+
+with open("output.html", "wb") as f:
+    f.write(html.encode("UTF-8"))
